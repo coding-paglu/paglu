ff65a32164252129d7cf711211d0841cc3f21d82
coding-paglu <divyamgdgps@gmail.com>
2025-08-26 18:37:06 +0530

    update

--- MatPlotLib/potty.py
+++ MatPlotLib/potty.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as mpl
 x=[1,2,3,4,5]
 y=[5,10,5,10,20]
 mpl.title('north')
-mpl.plot(x,y,color='blue',marker='o',mfc='k',mec='k',ms=10,linestyle='--',lw=3)
+mpl.plot(x,y,color='blue',marker='o',mfc='k',mec='k',ms=10,ls='--',lw=3)
 mpl.xlabel('south')
 mpl.ylabel('west')
 mpl.plot([2],[8],"g^")
